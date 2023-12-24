55f83018e7dc87f42ea5476dfb13211ad501d4b2
Maks Szokalski <42069493+illunix@users.noreply.github.com>
2023-12-24 22:26:39 +0100

    Add method to retrieve payment data

--- src/Axepta.SDK/Entities/Response/PaymentResponse.cs
+++ src/Axepta.SDK/Entities/Response/PaymentResponse.cs
@@ -3,8 +3,62 @@ namespace Axepta.SDK;
 public sealed record PaymentResponse
 {
     [JsonPropertyName("id")]
-    public required Guid Id { get; init; }
+    public required string Id { get; set; }
+
+    [JsonPropertyName("url")]
+    public string? Url { get; set; }
+
+    [JsonPropertyName("serviceId")]
+    public Guid? ServiceId { get; set; }
+
+    [JsonPropertyName("orderId")]
+    public string? OrderId { get; set; }
+
+    [JsonPropertyName("amount")]
+    public string? Amount { get; set; }
+
+    [JsonPropertyName("amountPaid")]
+    public int? AmountPaid { get; set; }
+
+    [JsonPropertyName("amountRefunded")]
+    public int? AmountRefunded { get; set; }
+
+    [JsonPropertyName("amountSubmittedRefund")]
+    public int? AmountSubmittedRefund { get; set; }
+
+    [JsonPropertyName("currency")]
+    public string? Currency { get; set; }
 
     [JsonPropertyName("status")]
-    public required string Status { get; init; }
+    public required OrderStatus Status { get; set; }
+
+    [JsonPropertyName("isActive")]
+    public bool? IsActive { get; set; }
+
+    [JsonPropertyName("createdAt")]
+    public int? CreatedAt { get; set; }
+
+    [JsonPropertyName("modifiedAt")]
+    public int? ModifiedAt { get; set; }
+
+    [JsonPropertyName("isGenerated")]
+    public bool? IsGenerated { get; set; }
+
+    [JsonPropertyName("isUsed")]
+    public bool? IsUsed { get; set; }
+
+    [JsonPropertyName("isConfirmVisited")]
+    public bool? IsConfirmVisited { get; set; }
+
+    [JsonPropertyName("returnUrl")]
+    public string? ReturnUrl { get; set; }
+
+    [JsonPropertyName("failureReturnUrl")]
+    public string? FailureReturnUrl { get; set; }
+
+    [JsonPropertyName("successReturnUrl")]
+    public string? SuccessReturnUrl { get; set; }
+
+    [JsonPropertyName("customer")]
+    public Customer? Customer { get; set; }
 }
